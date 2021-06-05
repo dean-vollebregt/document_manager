78509c5af96a30d36d9254985a1fb95f92d44e5d
deanvollebregt <dean.voll@gmail.com>
2021-06-05 13:14:31 +1000

    move aws client outside functions

--- lambdas/document_manager/services/s3_service.py
+++ lambdas/document_manager/services/s3_service.py
@@ -1,10 +1,10 @@
 import base64
 import boto3
 
+s3 = boto3.client('s3', region_name='ap-southeast-2')
+
 
 def upload_file(event):
-
-    s3 = boto3.client('s3', region_name='ap-southeast-2')
 
     try:
         response = s3.put_object(
@@ -18,8 +18,6 @@ def upload_file(event):
 
 
 def delete_file(event):
-
-    s3 = boto3.client('s3', region_name='ap-southeast-2')
 
     try:
         response = s3.delete_object(
