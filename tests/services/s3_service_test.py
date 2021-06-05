9f467e0fc5b8857026205cbe778b2d70bdb53fbe
deanvollebregt <dean.voll@gmail.com>
2021-06-05 15:23:25 +1000

    update s3 tests

--- tests/services/s3_service_test.py
+++ tests/services/s3_service_test.py
@@ -1,22 +1,20 @@
 import unittest
 
-from unittest import mock
+from unittest.mock import patch
+
 from lambdas.document_manager.services.s3_service import upload_file, delete_file
 
 
 class TestS3Services(unittest.TestCase):
 
-    def setUp(self):
-        self.s3_mock = mock
+    with patch('lambdas.document_manager.services.s3_service.boto3.client'):
+        def test_upload_file(self):
+            self.event = {"operation": "createS3Object", "fileName": "test.txt", "fileData": "aGVsbG8gd29ybGQK" }
+            response = upload_file(self.event)
+            assert response == None, 'Service should upload file to s3'
 
-    @mock.patch('lambdas.document_manager.services.s3_service.boto3.client')
-    def test_upload_file(self, s3_mock):
-        self.event = {"operation": "createS3Object", "fileName": "test.txt", "fileData": "aGVsbG8gd29ybGQK" }
-        response = upload_file(self.event)
-        assert response == None, 'Service should upload file to s3'
-
-    @mock.patch('lambdas.document_manager.services.s3_service.boto3.client')
-    def test_delete_file(self, s3_mock):
-        self.event = {"operation": "delete_file", "fileName": "delete.txt"}
-        response = delete_file(self.event)
-        assert response == 0, 'Service should delete object from S3'
+    with patch('lambdas.document_manager.services.s3_service.boto3.client'):
+        def test_delete_file(self):
+            self.event = {"operation": "delete_file", "fileName": "delete.txt"}
+            response = delete_file(self.event)
+            assert response == 0, 'Service should delete object from S3'
