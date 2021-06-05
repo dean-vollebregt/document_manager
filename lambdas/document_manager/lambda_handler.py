995cb0d80a350eeeabce54cadec7fb75e2aa82e1
deanvollebregt <dean.voll@gmail.com>
2021-06-05 11:20:51 +1000

    Move service tests to separate folder

--- lambdas/document_manager/lambda_handler.py
+++ lambdas/document_manager/lambda_handler.py
@@ -4,15 +4,20 @@ from services.dynamo_service import store_file_metadata, read_file_metadata, del
 
 def lambda_handler(event, context):
 
-    if event["operation"] == "upload_file":
-        return upload_file(event)
-    elif event["operation"] == "delete_file":
-        return delete_file(event)
-    elif event["operation"] == "store_file_metadata":
-        return store_file_metadata(event)
-    elif event["operation"] == "read_file_metadata":
-        return read_file_metadata()
-    elif event["operation"] == "delete_file_metadata":
-        return delete_file_metadata(event)
-    else:
-        print("Operation not found")
+    try:
+        if event["operation"] == "upload_file":
+            return upload_file(event)
+        elif event["operation"] == "delete_file":
+            return delete_file(event)
+        elif event["operation"] == "store_file_metadata":
+            return store_file_metadata(event)
+        elif event["operation"] == "read_file_metadata":
+            return read_file_metadata()
+        elif event["operation"] == "delete_file_metadata":
+            return delete_file_metadata(event)
+        else:
+            print("Operation not found")
+
+    except Exception as e:
+        print('Error processing event: ', e)
+
